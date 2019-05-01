5179f033aee51791c913d3a5c0225f12d1919fc7
ElDav1d <dvivoes@yahoo.es>
2019-05-01 10:26:01 +0200

    Create company class

--- Object_Oriented_Programming/Object_Inheritance_Company_Job_Vacants/main.js
+++ Object_Oriented_Programming/Object_Inheritance_Company_Job_Vacants/main.js
@@ -2,3 +2,18 @@ function Person(name, age) {
   this.name = name
   this.age = age
 }
+
+function Company(maxAge) {
+  this.maxAge = maxAge
+  this.nonSuitablePeople = nonSuitablePeople
+}
+
+function nonSuitablePeople(peopleAges) {
+  let counter = 0
+  for(let i = 0; i < peopleAges.length; i++) {
+    if(peopleAges[i] > this.maxAge) {
+      counter++
+    }
+  }
+  alert(`Cantidad de no aptos para la empresa: ${counter}`)
+}
