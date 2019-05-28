055ad7c02438edcec8838cc9c5a3e48e48cfaf75
ElDav1d <dvivoes@yahoo.es>
2019-05-28 08:32:03 +0200

    Apply regex non empty or whitespace string validation

--- Object_Oriented_Programming/Object_Inheritance_Company_Job_Vacants/main.js
+++ Object_Oriented_Programming/Object_Inheritance_Company_Job_Vacants/main.js
@@ -18,32 +18,52 @@ function nonSuitablePeople(peopleAges) {
   }
   alert(`Cantidad de no aptos para la empresa: ${counter}`)
 }
+
+const valueIsSet = (value) => value && !value.match(/^ *$/)
+
 const createCompany = function() {
-  let myCompanyName
-  let myCompanyMaxAge = 0
-  const valueIsSet = (value) => value && value !== ''
+  let myCompanyName, myCompanyVacants
+  let myCompanyMaxAge = ''
 
   while (!valueIsSet(myCompanyName)) {
     myCompanyName = prompt('Nombre de la empresa').toString()
   }
   
-  const myCompanyVacants = parseInt(prompt('¿Cuántas vacantes hay en la empresa?'))
+  while (!valueIsSet(myCompanyVacants)) {
+    myCompanyVacants = prompt('¿Cuántas vacantes hay en la empresa?')
+    parseInt(myCompanyVacants)
+  }
 
   if (myCompanyVacants === 0) {
     alert('Vaya...la cosa está mal, ¿no?')
     return
   } else {
-    myCompanyMaxAge = parseInt(prompt('Edad máxima de los empleados'))
+    while (!valueIsSet(myCompanyMaxAge)) {
+      myCompanyMaxAge = prompt('Edad máxima de los empleados')
+      parseInt(myCompanyMaxAge)
+    }
   }
-
+  
   myCompany = myCompanyName.toLowerCase()
   window[myCompany] = new Company(myCompanyVacants, myCompanyMaxAge)
-}const createApplicants = function(company) {
+  return
+}
+
+const createApplicants = function(company) {
+  let name, age
   let applicants = company.vacants
 
   while (applicants > 0) {
-    let name = prompt('Nombre')
-    let age = prompt('Edad')
+    while (!valueIsSet(name)) {
+      name = prompt('Nombre')
+    }
+
+    while (!valueIsSet(age)) {
+      age = prompt('Edad')
+      parseInt(age)
+    }
+    console.log(`Nombre ${applicants}: ${name}
+    Edad ${applicants}: ${age}`)
     window[name.toLowerCase()] = new Person(name, age)
     applicants--
   }
